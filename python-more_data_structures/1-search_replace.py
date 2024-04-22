3853161c267753f20cb05c56a48919d58b33e615
Harsha Vardhana <harsha@Harshas-MacBook-Pro-2.local>
2024-04-22 19:56:21 +0800

    Search and Replace

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -3,6 +3,7 @@ def search_replace(my_list, search, replace):
     new_list = list(map(lambda x: check_and_replace(x, search, replace), my_list))
     return new_list
 
+
 def check_and_replace(x, search, replace):
     if x == search:
         return replace
