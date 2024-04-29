4e5ff12b542bc3ae8ec8811106a4d5757e776a21
Harsha Vardhana <harsha@Harshas-MacBook-Pro-2.local>
2024-04-29 23:13:18 +0800

    Roman Numerals

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,6 +3,9 @@ def roman_to_int(roman_string):
     roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     ans = 0
     prev_value = 0
+    
+    if roman_string is None or isinstance(roman_string, str):
+        return ans
 
     for char in reversed(roman_string):
         value = roman[char]
