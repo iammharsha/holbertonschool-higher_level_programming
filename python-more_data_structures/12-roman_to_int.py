6fca5de0119b14296776f439a5045aeb06960f76
Harsha Vardhana <harsha@Harshas-MacBook-Pro-2.local>
2024-04-29 23:07:26 +0800

    Roman Numerals

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    ans = 0
+    prev_value = 0
+
+    for char in reversed(roman_string):
+        value = roman[char]
+        if value < prev_value:
+            ans -= value
+        else:
+            ans += value
+        prev_value = value
+
+    return ans
