ce2a998b8349e7dd700fcb899858fb834820943c
Harsha Vardhana <harsha@Harshas-MacBook-Pro-2.local>
2024-05-07 23:09:27 +0800

    Access and update private attribute

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -45,8 +45,8 @@ class Square:
         Args:
             value (int): Size of square
         """
-        if not isinstance(value, int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if value < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
