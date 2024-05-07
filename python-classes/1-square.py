d47212e41f4d127294e3b35390b479e1df81c375
Harsha Vardhana <harsha@Harshas-MacBook-Pro-2.local>
2024-05-07 22:45:20 +0800

    Square with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module for class Square"""
+
+
+class Square:
+    """
+    Definition of class Square
+   
+    Args:
+        size (int): Size of Square
+
+    """
+
+    __size = 0
+
+    def __init__(self, size):
+        self.__size = size
