63b219fa2099ef97119f2a119a9a7851fec79a1e
Harsha Vardhana <harsha@Harshas-MacBook-Pro-2.local>
2024-05-10 19:12:51 +0800

    Simple rectangle

---
+++ 0-rectangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+class Rectangle:
+    """A class to represent Rectangle Object"""
+
+
+    def __init__(self):
+        """Constructor for class Rectangle"""
+        pass
---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Definition and implementation of Class Rectangle"""
+
+
+class Rectangle:
+    """A class to represent Rectangle Object"""
+
+    def __init__(self):
+        """Constructor for class Rectangle"""
+        pass
