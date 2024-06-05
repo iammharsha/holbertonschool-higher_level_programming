7c5e92dc0f9477c096b0f53d98e5348bb43467d3
Harsha Vardhana <harsha@Harshas-MacBook-Pro-2.local>
2024-06-05 22:21:47 +0800

    Rectangle

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -2,7 +2,8 @@
 """Class Rectangle"""
 
 
-BaseGeometry = __import__(7-base_geometry).BaseGeometry
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
 
 class Rectangle(BaseGeometry):
     """Class Rectangle"""
