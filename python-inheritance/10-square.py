ae6a41761b302946791a5e6d1f256e22e04780cc
Harsha Vardhana <harsha@Harshas-MacBook-Pro-2.local>
2024-06-05 22:34:30 +0800

    Square

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Class Square"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Class Square"""
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """
+        Calculate Area of the square
+
+        Returns:
+            int: Returns the area of the Square
+        """
+        return self.__size * self.__size
