74eb8dc462bd6d2542b1a9da4da0dd97da19c281
Harsha Vardhana <harsha@Harshas-MacBook-Pro-2.local>
2024-06-29 17:43:20 +0800

    Display #1

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -156,8 +156,9 @@ class Rectangle(Base):
 
     def display(self):
         """Function to Print Rectangle using #"""
+        print("\n" * self.__y, end="")
         for _ in range(self.__height):
-            print('#' * self.__width)
+            print(' ' * self.__x + '#' * self.__width)
 
     def __str__(self):
         """
