fc9b55e23c76b4b87fef2926ed8c414dc33b573c
Harsha Vardhana <harsha@Harshas-MacBook-Pro-2.local>
2024-07-01 22:43:30 +0800

    Dictionary to JSON string

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Base Class"""
+import json
 
 
 class Base:
@@ -19,3 +20,18 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        Static Method to return JSON string from dictionary
+
+        Args:
+            list_dictionaries (list): A list of dictionaries
+
+        Returns:
+            str: JSON string representation of list_dictionaries
+        """
+        if list_dictionaries is None or not list_dictionaries:
+            return "[]"
+        return json.dumps(list_dictionaries)
