0db6a407581dc3435d0e26cd2a06aab7cfce55b4
root <harshavardhana.manimudi@gmail.com>
2024-07-08 09:35:36 +0000

    Task 12

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,15 @@
+-- List all shows and it's genre
+-- If it's not linked to a genre, it must be displayed as NULL
+-- List must be shown in ascending order of tv_shows.title and tv_show_genres.genre_id
+SELECT 
+    tv_shows.title,
+    tv_show_genres.genre_id
+FROM
+    tv_shows
+LEFT JOIN
+    tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+WHERE
+    tv_show_genres.genre_id IS NULL
+ORDER BY
+    tv_shows.title, tv_show_genres.genre_id
+ASC;
