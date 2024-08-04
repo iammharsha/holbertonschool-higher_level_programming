86e9e47123924e181e7341ddbbbf44213ed428a0
Harsha Vardhana <harsha@Harshas-MacBook-Pro-2.local>
2024-08-04 16:49:09 +0800

    Task 0

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -14,5 +14,5 @@ if __name__ == "__main__":
     url = sys.argv[1]
     req = urllib.request.Request(url)
     with urllib.request.urlopen(req) as response:
-        x_request_id = response.headers.get('X-Request-Id'):
+        x_request_id = response.headers.get('X-Request-Id')
         print(x_request_id)
--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
     headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'}
     r = requests.get('https://intranet.hbtn.io/status', headers=headers)
     print("Body response:")
-    print("\t- type:", type(r.content))
-    print("\t- content:", r.content)
+    print("\t- type:", type(r.text))
+    print("\t- content:", r.text)
